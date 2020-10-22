1cc8ab481ea25edbd702bf4d9520e33955fff2ba
hozerpffp <hozerpffp2009@gmail.com>
2020-10-22 18:14:14 -0500

    Updates

---
+++ precache-manifest.55aebd965520a8dee5eeac12a38cb9ef.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a2838903caff35acd029caf7f86746",
+    "url": "/%7Bgordo-flaca%7D/index.html"
+  },
+  {
+    "revision": "2ee89f9d3bf788861414",
+    "url": "/%7Bgordo-flaca%7D/static/css/main.ac080783.chunk.css"
+  },
+  {
+    "revision": "550c8489500bd2952a83",
+    "url": "/%7Bgordo-flaca%7D/static/js/2.0b803433.chunk.js"
+  },
+  {
+    "revision": "168183d1e7f7ddd4e227a6c479f4ebac",
+    "url": "/%7Bgordo-flaca%7D/static/js/2.0b803433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ee89f9d3bf788861414",
+    "url": "/%7Bgordo-flaca%7D/static/js/main.2f4f56fd.chunk.js"
+  },
+  {
+    "revision": "8ec92fb7757cf064a1cb",
+    "url": "/%7Bgordo-flaca%7D/static/js/runtime-main.59db6fb6.js"
+  },
+  {
+    "revision": "81eaddb7f3d125f1a49ac02db0a0b925",
+    "url": "/%7Bgordo-flaca%7D/static/media/oldHome.81eaddb7.JPG"
+  },
+  {
+    "revision": "70942b01077625a53858b7e4f4a4fe0e",
+    "url": "/%7Bgordo-flaca%7D/static/media/sam-sel.70942b01.jpeg"
+  }
+]);
